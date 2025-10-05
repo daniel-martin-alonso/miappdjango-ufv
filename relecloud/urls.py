46d17a3bc2a3e974c49a9bbba723a09beebec34d
Daniel Martin <dmartina@movistar.es>
2025-10-06 00:37:35 +0200

    Proyecto Django inicial listo para Azure

---
+++ relecloud/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('destinations', views.destinations, name='destinations'),
+    path('destination/<int:pk>', views.DestinationDetailView.as_view(), name='destination_detail'),
+    path('destination/add', views.DestinationCreateView.as_view(), name= 'destination_form'),
+    path('cruise/<int:pk>', views.CruiseDetailView.as_view(), name='cruise_detail'),
+    path('info_request', views.InfoRequestCreate.as_view(), name='info_request'),
+]
